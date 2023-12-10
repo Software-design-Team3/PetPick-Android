dcabf6f635812aef667884340f9be7b6e310df17
eJwtzD1vwyAUheH/wuwBxx8xbOnQoR+qKnXqgq7hOiYGQy6maRTlv9eNvB0d6XlvDLTGlL7ChDOTjO9rMEJUZdnu+L4zDUI1DJUWvBEdL/umGdqu7jgrWKTwYw3SRm9bSS1b6jgG5ceX8XyK41v2rciv7y0c5s98Pnz43Yl/19WzfaI19SBquUZcXY9A+P8mHR4HGG9nGXPvrFYTXgtCMDLQcR0xFDkhSfRgXXEJNA0uXFaMv9ESJgULk3N27n7/A+NnT5U= <kshyunn12@gmail.com>
2023-12-10 23:55:42 +0900

    UI : crown

--- PetPick/app/src/main/java/com/toyproject/petpick/adapter/InfoAdapter.kt
+++ PetPick/app/src/main/java/com/toyproject/petpick/adapter/InfoAdapter.kt
@@ -2,6 +2,7 @@ package com.toyproject.petpick.adapter
 
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.toyproject.petpick.R
@@ -17,6 +18,20 @@ class InfoAdapter(private val title : List<String>, private val context : List<S
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding.title.text = title[position]
         holder.binding.context.text = context[position]
+        when(context[position]){
+            " - 1위" ->{
+                holder.binding.crown.setImageResource(R.drawable.gold_crown)
+                holder.binding.crown.visibility = View.VISIBLE
+            }
+            " - 2위" ->{
+                holder.binding.crown.setImageResource(R.drawable.sliver_crown)
+                holder.binding.crown.visibility = View.VISIBLE
+            }
+            " - 3위" ->{
+                holder.binding.crown.setImageResource(R.drawable.bronze_crown)
+                holder.binding.crown.visibility = View.VISIBLE
+            }
+        }
 
         Log.d("InfoAdapter", "title$position : ${title[position]}\n context$position : ${context[position]}")
     }
